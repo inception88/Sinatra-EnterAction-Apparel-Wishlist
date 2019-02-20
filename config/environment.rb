c22044b42344882e5fdd7871c51dd41b4ea85635
Benjamin Jones <bljones36@gmail.com>
2019-02-20 03:52:17 +0000

    Added controller files

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,4 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "db/database.sqlite")
 
-# configure :development do
-# 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
-# end
 require_all 'app'
