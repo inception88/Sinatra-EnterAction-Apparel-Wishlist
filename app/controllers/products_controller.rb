e6d17439a1f3ab568f588074a71d2a163fd92ad3
Benjamin Jones <bljones36@gmail.com>
2019-07-11 03:14:04 +0000

    Updated product controller to prevent all route erros and redirect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,14 +22,22 @@ class ProductsController < ApplicationController
     if logged_in?
       @user = current_user
       @product = Product.find_by(id: params[:id])
-      if params[:list] == nil
-        redirect "/products/#{params[:id]}"
-      else
-        @wishlist = Wishlist.find(params[:list][:wishlists_ids])
-        if !@wishlist.products.include?(@product)
-          @wishlist.products << @product
+      if @product != nil
+        if params[:list] == nil
+          redirect "/products/#{params[:id]}"
+        else
+          @wishlist = @user.wishlists.find_by(id: params[:list][:wishlists_ids])
+          if @wishlist != nil
+            if !@wishlist.products.include?(@product)
+              @wishlist.products << @product
+            end
+            redirect "/wishlists/#{@wishlist.id}"
+          else
+            redirect "/products/#{params[:id]}"
+          end
         end
-        redirect "/wishlists/#{@wishlist.id}"
+      else
+        redirect '/products'
       end
     else
       redirect '/login'
